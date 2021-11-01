5662976e2e4e806557dcf4e59a4c64074264bb20
Elena <elenakudravceva2015@gmail.com>
2021-11-02 02:31:40 +0400

    reload calc

---
+++ Unit_2/calc/src/com/netcracker/domain/calculator/Calculated.java
@@ -0,0 +1,8 @@
+package com.netcracker.domain.calculator;
+
+public interface Calculated {
+    float add(float a, float b);
+    float minus(float a, float b);
+    float devide(float a, float b);
+    float multiple(float a, float b);
+}
