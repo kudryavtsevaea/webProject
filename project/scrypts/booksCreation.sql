976c607dd0a9a55f3a41b02f5539bc7693584258
Elena <elenakudravceva2015@gmail.com>
2022-01-23 20:27:31 +0400

    .

---
+++ project/scrypts/booksCreation.sql
@@ -0,0 +1,29 @@
+insert book(author, nameOfBook, yearOfPublishing, amountOfPages) 
+values ('А.С. Пушкин', 'Капитанская дочка', 1836, 384);
+
+insert book(author, nameOfBook, yearOfPublishing, amountOfPages) 
+values ('А.С. Пушкин', 'Евгений Онегин', 1833, 448);
+
+insert book(author, nameOfBook, yearOfPublishing, amountOfPages) 
+values ('Г. Гессе', 'Сиддхартха', 1922, 152);
+
+insert book(author, nameOfBook, yearOfPublishing, amountOfPages) 
+values ('Д. Керуак', 'Бродяги Дхармы', 1958, 187);
+
+insert book(author, nameOfBook, yearOfPublishing, amountOfPages) 
+values ('Г. Флобер', 'Госпожа Бовари', 1856, 384);
+
+insert book(author, nameOfBook, yearOfPublishing, amountOfPages) 
+values ('Л.Н. Толстой', 'Анна Каренина', 1877, 800);
+
+insert book(author, nameOfBook, yearOfPublishing, amountOfPages) 
+values ('В.В. Набоков', 'Лолита', 1955, 416);
+
+insert book(author, nameOfBook, yearOfPublishing, amountOfPages) 
+values ('А. Дюма', 'Три мушкетёра', 1844, 680);
+
+insert book(author, nameOfBook, yearOfPublishing, amountOfPages) 
+values ('М.А. Булгаков', 'Собачье сердце', 1925, 320);
+
+insert book(author, nameOfBook, yearOfPublishing, amountOfPages) 
+values ('П. Зюскинд', 'Парфюмер', 1985, 320);
